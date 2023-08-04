92df3c4342d70ca71228fcd8e98ccf972cd28481
Melroy Neil Dsouza <meldsza@gmail.com>
2023-08-04 14:39:42 +0530

    Update list_files.py

--- app/util/list_files.py
+++ app/util/list_files.py
@@ -1,6 +1,6 @@
 
 from datetime import datetime
-from typing import Optional
+from typing import Optional, List
 import app.models as models
 
 def list_files(prefix:Optional[str]=None, changes_since:Optional[datetime]=None) -> List[models.File]:
